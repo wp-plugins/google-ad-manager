7a59c3a1c42247bfea53548816d8383eb0488c46
stevegrunwell <stevegrunwell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-11 18:21:07 +0000

    Initial commit, complete development history available at https://github.com/stevegrunwell/google-ad-console
    git-svn-id: https://plugins.svn.wordpress.org/google-ad-manager/trunk@1197088 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ google-ad-console.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Plugin Name: Google Ad Console
+ * Plugin URI: https://stevegrunwell.com/blog/google-ad-console
+ * Description: Toggle the Google Ad Console widget within the WordPress admin bar.
+ * Version: 0.1
+ * Author: Steve Grunwell
+ * Author URI: https://stevegrunwell.com
+ *
+ * @package Google Ad Console
+ * @author Steve Grunwell
+ */
+
+/**
+ * Add the "Google Ads Console" item to the WordPress admin bar.
+ *
+ * @param WP_Admin_Bar $wp_admin_bar A reference to the global $wp_admin_bar object.
+ */
+function googleadconsole_add_node( $wp_admin_bar ) {
+
+	// Don't display this in the admin area
+	if ( is_admin() ) {
+		return;
+	}
+
+	$url  = googleadconsole_toggle_console_url();
+	$args = array(
+		'id'    => 'google-ad-console-toggle',
+		'title' => esc_html__( 'Google Ad Console', 'google-ad-console' ),
+		'href'  => esc_url( $url ),
+	);
+
+	// Active state for the node
+	if ( get_query_var( 'google_force_console', false ) ) {
+		$args['meta'] = array(
+			'class' => 'hover',
+		);
+	}
+
+	/**
+	 * Filter the arguments used to construct the "Google Ads Console" admin bar node.
+	 *
+	 * @param array $args WP admin bar node arguments.
+	 */
+	$args = apply_filters( 'googleadconsole_before_add_node', $args );
+
+	$wp_admin_bar->add_node( $args );
+}
+
+add_action( 'admin_bar_menu', 'googleadconsole_add_node', 99 );
+
+/**
+ * Register the ?google_force_console var within WordPress.
+ *
+ * @param array $vars Registered query vars.
+ * @return array The $vars array with a row for 'google_force_console' appended.
+ */
+function googleadconsole_register_query_var( $vars ) {
+	$vars[] = 'google_force_console';
+
+	return $vars;
+}
+
+add_filter( 'query_vars', 'googleadconsole_register_query_var' );
+
+/**
+ * Get the URL for the console toggle link.
+ *
+ * @return string The current URL with the console visible or hidden, depending on its current
+ *                state (e.g. whatever it isn't currently).
+ */
+function googleadconsole_toggle_console_url() {
+	$make_visible = ! get_query_var( 'google_force_console', false );
+
+	return add_query_arg( 'google_force_console', $make_visible );
+}
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Google Ad Console ===
+Contributors: stevegrunwell
+Tags: Google Publisher Tags, ads, debugging
+Requires at least: 4.2
+Tested up to: 4.2
+Stable tag: 0.1
+License: MIT
+License URI: http://opensource.org/licenses/MIT
+
+Add a toggle for the Google Publisher Console to the WordPress admin bar.
+
+== Description ==
+
+This plugin adds a simple "Google Ad Console" link to the WordPress admin bar, enabling logged-in users to easily toggle the [Google Publisher Console](https://support.google.com/dfp_sb/answer/181070?hl=en) when troubleshooting ad placements using Google Publisher Tags on your site.
+
+You can [keep up with the development of this plugin on GitHub](https://github.com/stevegrunwell/google-ad-console).
+
+*Please note that this plugin is in no way affiliated with Google Inc. or its subsidiaries.*
+
+== Installation ==
+
+1. Install the plugin through the WordPress plugin installer or by uploading the plugin .zip file to wp-content/plugins/
+2. Activate the plugin through the WordPress admin panel
+
+== Changelog ==
+
+= 0.1 =
+* Initial public release.
